1cb4ba88c961d92955ccd5272a7d200bab01c17f
afanasievov <afanasievov@gmail.com>
2016-11-02 22:47:16 +0200

    Added test folder.

---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+suite('App', () => {
+  test('Global object backboneTask should be present', () => {
+    assert.ok(window.backboneTask);
+  });
+});
---
+++ test/setup.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const assert = chai.assert;
+
+mocha.setup({
+  ui: 'tdd',
+  globals: ['backboneTask', 'gapi', '___jsl']
+});
